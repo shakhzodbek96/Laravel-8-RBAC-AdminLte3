8417243f146c3ccc79d43f486b10a8ca36458041
shakhzodbek96 <shakhzodbek96@mail.ru>
2021-04-30 16:51:59 +0500

    Fix initial client bug

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -66,14 +66,15 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
+        // check if new coming user is first or single
+        $cnt = User::count();
+        
         $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
-
-        // check if new coming user is first or single
-        $cnt = User::count();
+        
         if ($cnt === 0)
         {
             $perms_cnt = Permission::count();
